455d74c502f3a89c837e969e9d3cd0675353988f
Mirco Schmid <mirco.schmid@edu.teko.ch>
2021-09-12 15:56:34 +0200

    test

---
+++ Scripts/vbzdata.sql
@@ -0,0 +1,9 @@
+-- Create a 1:C example with auto and person
+
+DROP SCHEMA IF EXISTS vbzdata;
+-- In MySQL, the schema is the synonym for the database
+-- DROP DATABASE [IF EXISTS] database_name;
+
+CREATE SCHEMA `vbzdata` DEFAULT CHARACTER SET utf8mb4 ;
+USE vbzdata;
+
